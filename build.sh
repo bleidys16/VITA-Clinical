390d4c0d50c4104798306e23e688cc01bb8425d6
bleidys16 <lariosbleidys@gmail.com>
2026-06-15 20:25:17 -0500

    fix: agregar crear_usuarios_base al build.sh para deploy

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r backend/requirements.txt
 
-python backend/manage.py collectstatic --noinput
 python backend/manage.py migrate --noinput
+python backend/manage.py collectstatic --noinput
+python backend/manage.py crear_usuarios_base
